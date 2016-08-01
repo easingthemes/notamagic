4d7b690c92fed6141171a5210aad27f426b3ba79
Dragan Filipovic <dragan.filipovic@namics.com>
2016-08-01 18:53:40 +0200

    [new] utils: smooth scroll added

---
+++ src/utils/animateScroll.js
@@ -0,0 +1,14 @@
+const animateScroll = () => {
+	$(document).on('click', '.navbar-op ul li a, .navbar-op a.navbar-brand, .intro-direction a, a.go-to-top', function(event) {
+		event.preventDefault();
+		var hash = this.hash;
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
+			window.location.hash = hash;
+		});
+	});
+};
+
+export default animateScroll;
