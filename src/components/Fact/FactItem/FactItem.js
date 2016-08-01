4b1dac336839c6506d00e1e97a89a7b619a8678e
Dragan Filipovic <dragan.filipovic@namics.com>
2016-08-01 20:05:20 +0200

    [bugfix] Production react-optimise preset fixed fit static export

--- src/components/Fact/FactItem/FactItem.js
+++ src/components/Fact/FactItem/FactItem.js
@@ -9,34 +9,18 @@ import Counter from 'components/Counter';
  * @class FactItem
  * @extends ReactApp
  */
-export class FactItem extends React.Component {
-
-	//------------------------------------------------------------------------------------------------------------------
-	// Render methods
-	//------------------------------------------------------------------------------------------------------------------
-
-	/**
-	 * Renders the component
-	 *
-	 * @method render
-	 * @returns {XML}
-	 * @public
-	 */
-	render () {
-		return (
-			<div className="fact">
-				<div className="fact-number timer">
-					<span className="factor color-light">
-						<Counter id={'counter-' + this.props.id} to={this.props.number} />
-					</span>
-				</div>
-				<span className="fact-title color-light alpha7">
-					{this.props.name}
-				</span>
-			</div>
-		);
-	}
-}
+export const FactItem = () =>  (
+	<div className="fact">
+		<div className="fact-number timer">
+			<span className="factor color-light">
+				<Counter id={'counter-' + this.props.id} to={this.props.number} />
+			</span>
+		</div>
+		<span className="fact-title color-light alpha7">
+			{this.props.name}
+		</span>
+	</div>
+);
 
 FactItem.propTypes = {
 	id: React.PropTypes.number,
