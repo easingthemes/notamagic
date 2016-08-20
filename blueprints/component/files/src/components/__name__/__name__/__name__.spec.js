474e34d8f0db1727a67a7e2a8073bc5562e14a75
Dragan Filipovic <dragan.filipovic@namics.com>
2016-08-21 00:47:02 +0200

    [bugfix] blueprint name fix

--- blueprints/component/files/src/components/__name__/__name__/__name__.spec.js
+++ blueprints/component/files/src/components/__name__/__name__/__name__.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
-import FactItem from 'components/<%= pascalEntityName %>';
+import <%= pascalEntityName %> from 'components/<%= pascalEntityName %>';
 
 function shallowRender (component) {
 	const renderer = TestUtils.createRenderer();
