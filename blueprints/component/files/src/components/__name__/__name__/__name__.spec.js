712d4c037686e5e07caf809e6c5debc287f79be0
Dragan Filipovic <dragan.filipovic@namics.com>
2016-08-21 00:43:34 +0200

    [new] add test files to blueprints

---
+++ blueprints/component/files/src/components/__name__/__name__/__name__.spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import FactItem from 'components/<%= pascalEntityName %>';
+
+function shallowRender (component) {
+	const renderer = TestUtils.createRenderer();
+
+	renderer.render(component);
+	return renderer.getRenderOutput();
+}
+
+function shallowRenderWithProps (props = {}) {
+	return shallowRender(<<%= pascalEntityName %> {...props} />);
+}
+
+describe('(Component) <%= pascalEntityName %>', () => {
+	let _component;
+	let _props;
+
+	beforeEach(function () {
+		// _props = {
+		// 	location: {
+		// 		pathname: '/'
+		// 	}
+		// };
+		_props = {};
+
+		_component = shallowRenderWithProps(_props);
+	});
+
+	it('should have loaded.', () => {
+		expect(<%= pascalEntityName %>).to.exist;
+	});
+
+	it('should render without problems', () => {
+		expect(_component).to.exist;
+	});
+
+	it('should render as a <div>.', () => {
+		expect(_component.type).to.equal('div');
+	});
+});
